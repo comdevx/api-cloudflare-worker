de3864deea20098410bdcb777ac463a9ceb21184
comdevx <comdevx@gmail.com>
2021-09-17 19:09:04 +0700

    reverse code and updated header

--- helpers/response.js
+++ helpers/response.js
@@ -1,4 +1,5 @@
 module.exports = (ctx, data, status = 200) => {
+    ctx.response.headers = { 'Content-Type': 'application/json' }
     ctx.body = JSON.stringify(data)
     ctx.status = status
     return ctx
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const Router = require('./src')
+const Router = require('cloudworker-router')
 const { response } = require('./helpers')
 
 const router = new Router()
 
 router.get('/', async (ctx) => {
     const data = { msg: 'test' }
-    // response(ctx, data)
-    ctx.body = data
+    response(ctx, data)
 })
 
 addEventListener('fetch', event => {
